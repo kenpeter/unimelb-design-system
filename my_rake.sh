fdbe302b413fd5cdc37870b8717ddc1b7876caa2
kenpeter <figo2476@hotmail.com>
2015-05-27 14:27:24 +1000

    update

--- my_rake.sh
+++ my_rake.sh
@@ -1,8 +1,4 @@
 rm ./build -rf
 
 # http://stackoverflow.com/questions/6588674/what-does-bundle-exec-rake-mean
-<<<<<<< HEAD
-bundle exec rake assets:compile VERSION=latest
-=======
-bundle exec rake assets:compile VERSION=0.8
->>>>>>> 566c57065e99c4744fe20623650bd58872e78cc6
+bundle exec rake assets:compile VERSION=master
